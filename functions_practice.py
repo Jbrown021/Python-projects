e3217c39427afe8c5cefd9e63de5501d8b40573d
Jbrown021 <91687401+Jbrown021@users.noreply.github.com>
2022-07-02 12:44:23 -0400

    Create functions_practice.py

---
+++ functions_practice.py
@@ -0,0 +1,24 @@
+def hello():
+    print("hello, user")
+
+def pack(a,b,c):
+    return [a,b,c]
+
+
+def eat_lunch(my_list):
+    if len(my_list) == 0:
+        print("My lumchbox is empty!")
+    else:
+        for i in range(len(my_list)):
+            if i == 0:
+                print(f"First i eat {my_list[0]}")
+            else:
+                print(f"Next i eat {my_list[i]}")
+
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
